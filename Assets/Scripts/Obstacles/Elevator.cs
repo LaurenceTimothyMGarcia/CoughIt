7d75a9be115c58089d45da38d8798a7c1fdf194a
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2023-04-22 23:25:45 -0700

    added in physics for chair and elevator

--- Assets/Scripts/Obstacles/Elevator.cs
+++ Assets/Scripts/Obstacles/Elevator.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
+    // Speed elevator goes up
+    public float elevatorSpeed;
+    // Height elevator goes up to
+    public float elevatorHeight;
+
+    public Rigidbody rb;
+
+    private bool isFlying;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isFlying = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (rb.velocity.y == 0)
+        {
+            isFlying = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player" && !isFlying)
+        {
+            rb.AddForce(new Vector3(0, elevatorSpeed, 0), ForceMode.Impulse);
+            isFlying = true;
+        }
     }
 }
---
+++ Assets/Scripts/Obstacles/RollingObject.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingObject : MonoBehaviour
+{
+    // How fast object rolls
+    public float rollSpeed;
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Vector3 playerDir = col.transform.position - new Vector3(rb.transform.position.x, col.transform.position.y, rb.transform.position.z);
+            playerDir = playerDir.normalized;
+            rb.AddForce((playerDir * rollSpeed), ForceMode.Impulse);
+        }
+    }
+}
