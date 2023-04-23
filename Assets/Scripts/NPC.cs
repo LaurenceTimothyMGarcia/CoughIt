645fad3ba4227490d78ed09ac8e004014503358b
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2023-04-23 16:47:50 -0700

    fixed up animations

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -86,7 +86,7 @@ public class NPC : MonoBehaviour
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
         walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-    
+
         if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
         {
             walkPointSet = true;
